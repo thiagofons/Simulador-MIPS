339ec2fedb077e7c475b82ad9a9443c3914bdef5
Thiago Arruda <thiago_arruda@id.uff.br>
2022-07-08 19:42:50 -0300

    Deu em nada kk

---
+++ script.js
@@ -0,0 +1,32 @@
+// Coisas do DOM
+
+
+// Dados
+const instrucao = ["IF" , "ID", "EX", "MEM", "WB"];
+const instrucoes = [
+    "add",
+    "addi",
+    "and",
+    "beq",
+    "bne",
+    "j",
+    "jal",
+    "jr",
+    "lw",
+    "or",
+    "sll",
+    "srl",
+    "sw",
+    "sub"
+];
+const memoriaDados = [];
+const bancoRegistradores = [];
+
+// Logica
+    // 1 - Receber o arquivo ASM por texto
+    
+
+
+
+
+
