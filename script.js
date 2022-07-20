37950976793ce8661da2962626f3c1808d3a180b
OverMan-647 <brunoadji@id.uff.br>
2022-07-20 03:56:36 -0300

    dor e sofrimento

--- script.js
+++ script.js
@@ -14,40 +14,48 @@ window.onload = () => {
 };
 
 // Dados
+let stallJump = false;
+let linha = 0;
+let ciclo = 0;
+let bolha = 0;
+let finish = 1;
+
+let fix = 0;
+
 let numeroInstrucoes = 0;
 var registradores = {
-    "$zero": 0,
-    "$at": 0,
-    "$v0": 0,
-    "$v1": 0,
-    "$a0": 0,
-    "$a1": 0,
-    "$a2": 0,
-    "$a3": 0,
-    "$t0": 0,
-    "$t1": 0,
-    "$t2": 0,
-    "$t3": 0,
-    "$t4": 0,
-    "$t5": 0,
-    "$t6": 0,
-    "$t7": 0,
-    "$s0": 0,
-    "$s1": 0,
-    "$s2": 0,
-    "$s3": 0,
-    "$s4": 0,
-    "$s5": 0,
-    "$s6": 0,
-    "$s7": 0,
-    "$t8": 0,
-    "$t9": 0,
-    "$k0": 0,
-    "$k1": 0,
-    "$gp": 0,
-    "$sp": 0,
-    "$fp": 0,
-    "$ra": 0
+    "$zero": [0, -1, 0],
+    "$at": [0, -1, 0],
+    "$v0": [0, -1, 0],
+    "$v1": [0, -1, 0],
+    "$a0": [0, -1, 0],
+    "$a1": [0, -1, 0],
+    "$a2": [0, -1, 0],
+    "$a3": [0, -1, 0],
+    "$t0": [0, -1, 0],
+    "$t1": [0, -1, 0],
+    "$t2": [0, -1, 0],
+    "$t3": [0, -1, 0],
+    "$t4": [0, -1, 0],
+    "$t5": [0, -1, 0],
+    "$t6": [0, -1, 0],
+    "$t7": [0, -1, 0],
+    "$s0": [0, -1, 0],
+    "$s1": [0, -1, 0],
+    "$s2": [0, -1, 0],
+    "$s3": [0, -1, 0],
+    "$s4": [0, -1, 0],
+    "$s5": [0, -1, 0],
+    "$s6": [0, -1, 0],
+    "$s7": [0, -1, 0],
+    "$t8": [0, -1, 0],
+    "$t9": [0, -1, 0],
+    "$k0": [0, -1, 0],
+    "$k1": [0, -1, 0],
+    "$gp": [0, -1, 0],
+    "$sp": [0, -1, 0],
+    "$fp": [0, -1, 0],
+    "$ra": [0, -1, 0]
 }
 let textoCodigo = "";
 
@@ -71,6 +79,7 @@ const instrucoes = [
 let labels = {};
 let memoriaDados = {};
 let bancoRegistradores = [];
+let registradoresModificar = [];
 
 
 // Logica
@@ -83,8 +92,9 @@ inserirCodigo.addEventListener("click", () => {
 function submit() {
     resetaDiagrama();
     carregarDiagrama();
-    textoCodigo = document.getElementById("codigo").value.split("\n").filter(checkNaoVazio);
-    geraLinhas();
+    textoCodigo = document.getElementById("codigo").value.split("\n").filter(checkLinhas);
+    //geraLinhas();
+    textoOriginal = [...textoCodigo];
     textoCodigo = processaLinhas(textoCodigo);
     leLabel();
     document.getElementById("codigo").value = "";
@@ -124,9 +134,20 @@ function checkInst(cmp) {
     return -1;
 }
 
-function atualizaRegistrador(registrador, valorNovo) {
-    document.getElementById(registrador).innerHTML = valorNovo;
-    registradores[registrador] = valorNovo;
+function atualizaRegistrador(registrador, valorNovo, ciclos) {
+    registradoresModificar.push(registrador);
+    registradores[registrador][1] = ciclos;
+    registradores[registrador][2] = valorNovo;
+}
+
+function controlaRegistrador(registrador, i) {
+    registradores[registrador][1]--;
+    if(registradores[registrador][1] == 0) {
+        registradores[registrador][0] = registradores[registrador][2];
+        document.getElementById(registrador).innerHTML = registradores[registrador][0]
+        registradoresModificar.splice(i, 1);
+        fix++;
+    }
 }
 
 function insereMemoria(memoriaNova, valor=0) {
@@ -142,18 +163,9 @@ function modificaMemoria(memoria, valorNovo) {
 
 // Conversao do codigo no diagrama
 function carregarDiagrama() {
-    let linhaAtual = 0;
-
     // Carregar o cabecalho do diagrama
     const cabecalho = document.getElementById("tab-cabecalho");
     cabecalho.innerHTML = `<th>instrução</th>`;
-    for(let i = 1; i < numeroInstrucoes + 1; i++) {
-        cabecalho.innerHTML += `<th>${i}</th>`;
-    }
-    linhaAtual++;
-
-    // Carregar as instrucoes
-
 }
 
 function carregarBancoRegistradores() {
@@ -165,7 +177,7 @@ function carregarBancoRegistradores() {
     `
     valorReg = Object.entries(registradores);
     for(let i = 0; i < valorReg.length; i++) {
-        loadReg += `\n<tr><td>${valorReg[i][0]}</td><td id = ${valorReg[i][0]}>${valorReg[i][1]}</td></tr>`;
+        loadReg += `\n<tr><td>${valorReg[i][0]}</td><td id = ${valorReg[i][0]}>${valorReg[i][1][0]}</td></tr>`;
     }
     bancoReg.innerHTML = loadReg;
 }
@@ -178,48 +190,130 @@ function carregarMemoria() {
     </tr>`;
 }
 
-function contaInstrucao() {
-    let i = 0;
-    let linhaJal = 0;
-    while(i < textoCodigo.length) {
-        if(checkInst(textoCodigo[i][0]) != -1) {
+function passaCiclo() {
+    ciclo++;
+    if(finish > 4) return;
+    for(let i = 0; i-fix < registradoresModificar.length; i++) {
+        console.log(registradoresModificar[i-fix]);
+        controlaRegistrador(registradoresModificar[i-fix], i-fix);
+    }
+    fix = 0;
+    if(linha >= textoCodigo.length) {
+        finalizaPrograma(finish);
+        finish++;
+        return;
+    };
+    
+    const cabecalho = document.getElementById("tab-cabecalho")
+    cabecalho.innerHTML += `<th>${ciclo}</th>`;
+    
+    tamanhoTabela = diagrama.rows.length;
+    if(bolha > 0) {
+        let inicio = `
+        <tr class="tab-linha instrucao" id = "${ciclo}">
+        <td class="nome-instrucao" id = "codigo${ciclo}">sll $zero, $zero, 0</td>`
+
+        for(let espaco = 0; espaco < ciclo - 1; espaco++) {
+            inicio += `<td></td>`;
+        }
+        inicio += `<td id = "if${ciclo}">IF</td>`
+        diagrama.insertRow(tamanhoTabela).outerHTML = inicio;
+        for(let i = ciclo-1; i > ciclo-5 && i > 0; i--) {
+            switch(i) {
+                case ciclo-1: 
+                    document.getElementById(`${i}`).innerHTML += `<td id = "id${ciclo}">ID</td>`
+                    break;
+                
+                case ciclo-2:
+                    document.getElementById(`${i}`).innerHTML += `<td id = "ex${ciclo}">EX</td>`
+                    break;
+                
+                case ciclo-3:
+                    document.getElementById(`${i}`).innerHTML += `<td id = "mem${ciclo}">MEM</td>`
+                    break;
+                
+                case ciclo-4:
+                    document.getElementById(`${i}`).innerHTML += `<td id = "wb${ciclo}">WB</td>`
+                    break;
+            }
+        }
+        bolha--;
+    } else {
+        let inicio = `
+        <tr class="tab-linha instrucao" id = "${ciclo}">
+        <td class="nome-instrucao" id = "codigo${ciclo}">${textoOriginal[linha]}</td>`
+
+        for(let espaco = 0; espaco < ciclo - 1; espaco++) {
+            inicio += `<td></td>`;
+        }
+        inicio += `<td id = "if${ciclo}">IF</td>`
+        diagrama.insertRow(tamanhoTabela).outerHTML = inicio;
+        for(let i = ciclo-1; i > ciclo-5 && i > 0; i--) {
+            switch(i) {
+                case ciclo-1: 
+                    document.getElementById(`${i}`).innerHTML += `<td id = "id${ciclo}">ID</td>`
+                    break;
+                
+                case ciclo-2:
+                    document.getElementById(`${i}`).innerHTML += `<td id = "ex${ciclo}">EX</td>`
+                    break;
+                
+                case ciclo-3:
+                    document.getElementById(`${i}`).innerHTML += `<td id = "mem${ciclo}">MEM</td>`
+                    break;
+                
+                case ciclo-4:
+                    document.getElementById(`${i}`).innerHTML += `<td id = "wb${ciclo}">WB</td>`
+                    break;
+            }
+        }
+        let linhaJal = 0;
+        if(checkInst(textoCodigo[linha][0]) != -1) {
             numeroInstrucoes += 1;
-            switch(textoCodigo[i][0]) {
+            switch(textoCodigo[linha][0]) {
                 case "beq":
-                    if(registradores[textoCodigo[i][1]] == registradores[textoCodigo[i][2]]) {
-                        i = labels[textoCodigo[i][3]];
+                    if(registradores[textoCodigo[linha][1]][0] == registradores[textoCodigo[linha][2]][0]) {
+                        linha = labels[textoCodigo[linha][3]] - 1;
                     }
+                    if(stallJump)
+                        bolha = 1;
                     break;
                 
                 case "bne":
-                    if(registradores[textoCodigo[i][1]] != registradores[textoCodigo[i][2]]) {
-                        i = labels[textoCodigo[i][3]];
+                    if(registradores[textoCodigo[linha][1]][0] != registradores[textoCodigo[linha][2]][0]) {
+                        linha = labels[textoCodigo[linha][3]] - 1;
                     }
+                    if(stallJump)
+                        bolha = 1;
                     break;
                 
                 case "j":
-                    i = labels[textoCodigo[i][1]];
+                    linha = labels[textoCodigo[linha][1]] - 1;
+                    if(stallJump)
+                        bolha = 1;
                     break;
                 
                 case "jal":
-                    linhaJal = i;
-                    i = labels[textoCodigo[i][1]];
+                    linhaJal = linha;
+                    linha = labels[textoCodigo[linha][1]] - 1;
+                    bolha = 1;
                     break;
                 
                 case "jr":
-                    i = linhaJal + 1;
+                    linha = linhaJal;
+                    bolha = 1;
                     break;
                 
                 case "add":
-                    atualizaRegistrador(textoCodigo[i][1], registradores[textoCodigo[i][2]] + registradores[textoCodigo[i][3]]);
+                    atualizaRegistrador(textoCodigo[linha][1], registradores[textoCodigo[linha][2]][0] + registradores[textoCodigo[linha][3]][0], 3);
                     break;
                 
                 case "addi":
-                    atualizaRegistrador(textoCodigo[i][1], registradores[textoCodigo[i][2]] + parseInt(textoCodigo[i][3]));
+                    atualizaRegistrador(textoCodigo[linha][1], registradores[textoCodigo[linha][2]][0] + parseInt(textoCodigo[linha][3]), 3);
                     break;
                 
                 case "and":
-                    atualizaRegistrador(textoCodigo[i][1], registradores[textoCodigo[i][2]] & registradores[textoCodigo[i][3]]);
+                    atualizaRegistrador(textoCodigo[linha][1], registradores[textoCodigo[linha][2]][0] & registradores[textoCodigo[linha][3]][0], 3);
                     break;
                 
                 case "lw":
@@ -227,32 +321,67 @@ function contaInstrucao() {
                     break;
                 
                 case "or":
-                    atualizaRegistrador(textoCodigo[i][1], registradores[textoCodigo[i][2]] | registradores[textoCodigo[i][3]]);
+                    atualizaRegistrador(textoCodigo[linha][1], registradores[textoCodigo[linha][2]][0] | registradores[textoCodigo[linha][3]][0], 3);
                     break;
                 
                 case "sll":
-                    atualizaRegistrador(textoCodigo[i][1], registradores[textoCodigo[i][1]] << parseInt(textoCodigo[i][2]))
+                    atualizaRegistrador(textoCodigo[linha][1], registradores[textoCodigo[linha][1]][0] << parseInt(textoCodigo[linha][2]), 3)
                     break;
                 
                 case "srl":
-                    atualizaRegistrador(textoCodigo[i][1], registradores[textoCodigo[i][1]] >> parseInt(textoCodigo[i][2]))
+                    atualizaRegistrador(textoCodigo[linha][1], registradores[textoCodigo[linha][1]][0] >> parseInt(textoCodigo[linha][2]), 3)
                     break;
                 
                 case "sw":
-
+                    //???
                     break;
 
                 case "sub":
-                    atualizaRegistrador(textoCodigo[i][1], registradores[textoCodigo[i][2]] - registradores[textoCodigo[i][3]]);
+                    atualizaRegistrador(textoCodigo[linha][1], registradores[textoCodigo[linha][2]][0] - registradores[textoCodigo[linha][3]][0], 3);
                     break;
             }
+            linha++;
         }
-        i++;
     }
 }
 
-function checkNaoVazio(arr) {
-    if(arr.length > 0) return true;
+function finalizaPrograma(pos) {
+    const cabecalho = document.getElementById("tab-cabecalho")
+    cabecalho.innerHTML += `<th>${ciclo}</th>`;
+    switch(pos) {
+        case 1:
+            document.getElementById(`${ciclo-pos}`).innerHTML += `<td id = "id${ciclo-pos}">ID</td>`
+            
+            document.getElementById(`${ciclo-pos-1}`).innerHTML += `<td id = "ex${ciclo-pos-1}">EX</td>`
+            
+            document.getElementById(`${ciclo-pos-2}`).innerHTML += `<td id = "mem${ciclo-pos-2}">MEM</td>`
+
+            document.getElementById(`${ciclo-pos-3}`).innerHTML += `<td id = "wb${ciclo-pos-3}">WB</td>`
+            break;
+        
+        case 2:
+            document.getElementById(`${ciclo-pos}`).innerHTML += `<td id = "ex${ciclo-pos}">EX</td>`
+
+            document.getElementById(`${ciclo-pos-1}`).innerHTML += `<td id = "mem${ciclo-pos-1}">MEM</td>`
+            
+            document.getElementById(`${ciclo-pos-2}`).innerHTML += `<td id = "wb${ciclo-pos-2}">WB</td>`
+            break;
+        
+        case 3:
+            document.getElementById(`${ciclo-pos}`).innerHTML += `<td id = "mem${ciclo-pos}">MEM</td>`
+
+            document.getElementById(`${ciclo-pos-1}`).innerHTML += `<td id = "wb${ciclo-pos-1}">WB</td>`
+            break;
+
+        case 4:
+            document.getElementById(`${ciclo-pos}`).innerHTML += `<td id = "wb${ciclo-pos}">WB</td>`
+            break;
+
+    }
+}
+
+function checkLinhas(arr) {
+    if(arr.length > 0 && !arr[0].startsWith("#")) return true;
     return false
 }
 
@@ -287,10 +416,20 @@ function geraLinhas() {
 }
 
 function resetaDiagrama() {
+    registradoresModificar = [];
     diagrama.innerHTML = "<tr id='tab-cabecalho'></tr>";
     let temp = Object.entries(registradores);
+    linha = 0;
+    ciclo = 0;
+    finish = 1;
     for(let i = 0; i < temp.length; i++) {
-        atualizaRegistrador(temp[i][0], 0) ;
+        resetaRegistrador(temp[i][0]) ;
     }
 }
 
+function resetaRegistrador(registrador) {
+    registradores[registrador][0] = 0;
+    registradores[registrador][1] = -1;
+    registradores[registrador][2] = 0;
+    document.getElementById(registrador).innerHTML = registradores[registrador][0]
+}
